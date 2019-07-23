63b2e413db0ceb635c94565f386c7a523c2cec7e
Luigi <luigi.fiori.lf0303@gmail.com>
2019-07-23 11:23:26 +0100

    updated lab

--- school.py
+++ school.py
@@ -1,1 +1,23 @@
-class School:
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class School():
+    def __init__(self, name, roster = {}):
+        self.name = name
+        self.roster = roster
+
+    def add_student(self, name, grade):
+        if grade in self.roster:
+            self.roster[grade].append(name)
+        else:
+            self.roster[grade] = [name]
+
+    def grade(self, grade):
+        return self.roster[grade]
+
+    def sort_roster(self):
+        sorted_roster = sorted(self.roster.items(), key = lambda x: x[1])
+        print(sorted_roster)
